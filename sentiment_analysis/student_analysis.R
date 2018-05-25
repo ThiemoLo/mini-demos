a7f8a1580999ee4134270c209151de8fa50730a1
Thiemo Loos <thiemo@Thiemos-Air.local>
2018-05-24 23:40:07 -0700

    Week 9

--- sentiment_analysis/student_analysis.R
+++ sentiment_analysis/student_analysis.R
@@ -1,14 +1,18 @@
 # Student sentence analysis - look at sentiments of students' sentences
 
-# install.packages('syuzhet')
+install.packages('syuzhet')
 library(syuzhet)
 
 # Create a vector of emotional sentences. 
 # Add some happy ones, angry ones - you name it!
 student_sentences <- c('I really like the pie you gave me this morning.', 
                        'Your shoes suck and are just plain ugly.',
-                       'I\'d really truly love going out in this weather!'
+                       'I\'d really truly love going out in this weather!',
+                       'I really want to sleep right, I am tired',
+                       'This class has been pretty interesting'
                        )
 
 # Analyze sentiment for student sentences
-
+student_sentiments <- data.frame(get_sentiment(student_sentences, method ='syuzhet'))
+student_analysis <- cbind(sentence =  student_sentences, student_sentiments)
+View(student_analysis)
